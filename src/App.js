8870c610a12a912a369b0051421bf7aa56f4f40d
josenauto <jemilionautoch@gmail.com>
2021-01-20 18:30:39 -0300

    Fetched the jobs fromthe json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 const url = "https://josenauto.github.io/api/tabs-jobs-api.json";
 
 function App() {
-  return (
-    <h2>Tabs Project Setup</h2>
-  );
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(()=>{
+    fetchJobs();
+  },[])
+
+  if (loading) {
+    return <div>
+      <h1>Loading...</h1>
+    </div>
+  }
+
+  return <h2>Jobs</h2>;
 }
 
 export default App;
