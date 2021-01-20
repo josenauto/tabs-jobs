bbd60e1df52e35890abee40c058a2e959709eeb2
josenauto <jemilionautoch@gmail.com>
2021-01-20 20:12:05 -0300

    Added the buttons of companies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from 'react-icons/fa';
+import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://josenauto.github.io/api/tabs-jobs-api.json";
 
 function App() {
@@ -14,41 +14,54 @@ function App() {
     setLoading(false);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchJobs();
-  },[])
+  }, []);
 
   if (loading) {
-    return <div>
-      <h1>Loading...</h1>
-    </div>
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
 
-  const {company, dates, duties,title} = jobs[value];
+  const { company, dates, duties, title } = jobs[value];
 
-
-
-  return <div>
+  return (
     <div>
-      <h2>Experiencia</h2>
-
-    </div>
-    <div>
-      {/*btn container*/}
-      {/*job info*/}
       <div>
-        <h3>{title}</h3>
-        <h4>{company}</h4>
-        <p>{dates}</p>
-        {duties.map((duty,index)=>{
-          return <div key={index}>
-            <FaAngleDoubleRight></FaAngleDoubleRight>
-            <p>{duty}</p>
-          </div>
-        })}
+        <h2>Experiencia</h2>
+      </div>
+      <div>
+        {/*btn container*/}
+        <div>
+          {jobs.map((item, index) => {
+            return (
+              <button key={item.id} onClick={() => setValue(index)}>
+                {/* index = 0: Forcenter, 1: Paradigma, 2:Forsend*/}
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/*job info*/}
+        <div>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index}>
+                <FaAngleDoubleRight></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
